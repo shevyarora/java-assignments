953ddfd9a94cbbf3ca9e4b603affbf9136d9b765
shevyarora <shevykumar26@gmail.com>
2023-09-18 02:10:40 +0530

    Add files via upload

---
+++ java io - reading inputs in java assignment-2/q4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class q4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 1; i <= t; i++) {
+
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            System.out.println(num1 + num2);
+        }
+
+    }
+}
